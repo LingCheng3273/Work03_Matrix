66c8090dff22e32c64a30c853bdae38fadbc65d4
LingCheng3273 <lcheng3@stuy.edu>
2017-02-17 15:28:32 -0500

    working on add_edge

---
+++ main.py
@@ -0,0 +1,12 @@
+from display import *
+from draw import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+matrix = new_matrix()
+
+
+#draw_lines( matrix, screen, color )
+print_matrix(matrix)
+
+#display(screen)
