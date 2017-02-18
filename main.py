1ae9716f7b7609dff9959be883ccf831023ec774
LingCheng3273 <lcheng3@stuy.edu>
2017-02-17 20:28:10 -0500

    finished ident abd scalar_mult. Working on matrix_mult

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
 
-
+print("Here is your original matrix:")
 print_matrix(matrix)
 add_edge(matrix, 0, 0, 0, 500, 500, 500)
-draw_lines(matrix, screen, color)
+#draw_lines(matrix, screen, color)
+print("Here is your new matrix")
 print_matrix(matrix)
 
-display(screen)
+print("This the the multiplicative identity of your matrix")
+print_matrix(ident(matrix))
+
+print("Here is your matrix after *2")
+scalar_mult(matrix, 2)
+print_matrix(matrix)
+
+
+
+
+#display(screen)
--- matrix.py
+++ matrix.py
@@ -10,15 +10,30 @@ def print_matrix( matrix ):
     print pr
 
 def ident( matrix ):
-    pass
+    m=[]
+    for c in range( len(matrix) ):
+        m.append( [] )
+        for r in range(len(matrix)):
+            m[c].append(0)
+    for c in range( len(m) ):
+        m[c][c]=1
+    return m
 
 def scalar_mult( matrix, s ):
-    pass
+    for c in range(len(matrix)):
+        for r in range(len(matrix[0])):
+            matrix[c][r]= matrix[c][r] * s
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
-
+    r1= len(m1[0])
+    c1= len(m1)
+    r2= len(m2[0])
+    c2= len(m2)
+    for r in range(r1):
+        for c in range(c2):
+            m2[r][c]= m1[0][r]*m2[c][0] + m1[1][r]*m2[c][1] + m1[2][r]*m2[c][2] + m1[3][r]* m2[r][3]
+        
 
 
 
